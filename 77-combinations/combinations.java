4c5d7200b145dc33c95c15126ece595f765be0f7
Kartik11nikumbh <154928754+Kartik11nikumbh@users.noreply.github.com>
2026-02-05 23:47:35 +0530

    Time: 19 ms (53.14%) | Memory: 98 MB (81.06%) - LeetSync

---
+++ 77-combinations/combinations.java
@@ -0,0 +1,24 @@
+class Solution {
+   List<List<Integer>> result = new ArrayList<>();
+
+    public void backtrack(int start, int n, int k, List<Integer> temp) {
+
+        if (temp.size() == k) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+
+            temp.add(i);
+            backtrack(i + 1, n, k, temp);
+            temp.remove(temp.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> combine(int n, int k) {
+
+        backtrack(1, n, k, new ArrayList<>());
+        return result;
+    }
+}
